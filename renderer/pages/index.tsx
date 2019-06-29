129e29aaaea0a7616f44acfc26f37a8ad364c095
Stephen Yu <s.yu@less3.com>
2019-06-30 07:45:12 +1000

    Reading data from JSON file

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import audioRecords from "data/indexPage.json";
+
 import { AudioPlayer } from "audio_player/audio_player";
 import { AudioPlaylist, AudioRecord } from "audio_playlist/audio_playlist";
 
@@ -10,25 +12,7 @@ interface IndexPageProps {
 export default class IndexPage extends React.Component<IndexPageProps> {
   static getInitialProps() {
     return {
-      audioRecords: [
-        {
-          id: "bbc",
-          name: "BBC",
-          url:
-            "//open.live.bbc.co.uk/mediaselector/6/redir/version/2.0/mediaset/audio-nondrm-download-low/proto/https/vpid/w1msh27cqrdrcj4.mp3"
-        },
-        {
-          id: "cbc",
-          name: "CBC",
-          url: "http://podcast.cbc.ca/mp3/hourlynews.mp3"
-        },
-        {
-          id: "wsj",
-          name: "Wall Street Journal",
-          url:
-            "https://dcs.megaphone.fm/WSJ4093146914.mp3?key=a1119b23405fefa16c564ad6ac2f3262"
-        }
-      ]
+      audioRecords
     };
   }
   render() {
